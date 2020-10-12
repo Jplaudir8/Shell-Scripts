a9771aa37ddc06fe7feacd4cc512dfdad7021290
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-10-12 17:34:08 -0500

    added new shell script

---
+++ doBlankWebProject.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+projectPath="./newWebProject"
+
+mkdir $projectPath
+
+touch $projectPath"/index.html"
+
+mkdir $projectPath"/css"
+touch $projectPath"/css/main.css"
+
+mkdir $projectPath"/js"
+touch $projectPath"/js/main.js"
