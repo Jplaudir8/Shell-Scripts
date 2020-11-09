c3bef97abaa6a27c0a25557e7385d93244f5ad6d
Joan-Perez-Lozano <joanperezl123@gmail.com>
2020-11-09 11:20:45 -0500

    addded index content

--- doBlankWebProject.sh
+++ doBlankWebProject.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-echo "Write project name"
-read projectName
+echo "Make sure this shell is located wherever you want to generate this project."
+printf "Write project name: "
+read -r projectName
 
 projectPath="./"$projectName
 
 mkdir $projectPath
 
-touch $projectPath"/index.html"
+echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>" >> $projectPath"/index.html"
 
 mkdir $projectPath"/css"
 touch $projectPath"/css/main.css"
